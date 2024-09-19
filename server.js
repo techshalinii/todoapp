ddb29a33708e561cf653fb6a9ffa0644241a8c4b
techshalinii <shalinikumari9142@gmail.com>
2024-09-19 18:50:13 +0530

    updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(
-    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV || "Development"} Mode`
+    `Server is working on port:${process.env.PORT} `
   );
 });
+//in ${process.env.NODE_ENV || "Production"} Mode
--- utils/features.js
+++ utils/features.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
-
+//process.env.NODE_ENV === "Development" ? "lax" : 
+// process.env.NODE_ENV === "Development" ? false : 
 export const sendCookie = (user, res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
   res
@@ -7,8 +8,8 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Development" ? false : true,
+      sameSite: "none",
+      secure:true,
 
     })
     .json({
